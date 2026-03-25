8c9368fc4e2aaf1e6b88432b8ed6032d40bf276b
SRCreator <85744292+onu032001@users.noreply.github.com>
2026-03-25 17:47:26 +0900

    enhance tokenizer and added code snippets to parser
    I enhanced the tokenizer so that can handle whitespaces. I also added code snippets to the parser to parse expressions using extended backus-naur form. also, I added two special functions to the parser so users can parse expressions. one is exponent parsing and one is function parsing.

--- octo-calculator-by-neungsuksaeng.js
+++ octo-calculator-by-neungsuksaeng.js
@@ -1,24 +1,29 @@
 class NeungsuksaengCalculateTokenizer {
-  parse(expressionString) {
+  constructor() {
+    this.numberRegex = /^-?\d+(?:\.\d*)?$/g;
+    this.letterRegex = /^[A-Za-z_][A-Za-z\d_]*$/g;
+  }
+  tokenize(expressionString) {
     let index = 0;
     let tempToken = '';
     const result = [];
     while (index < expressionString.length) {
       const char = expressionString[index];
+      if (char === ' ') continue;
       if (/\d/.test(char)) {
-        if (!(/^(?:-?\d+(?:.\d*)?|\w(?:\w|\d)*)$/.test(tempToken)) && tempToken !== '') {
+        if (!(this.numberRegex.test(tempToken) || this.letterRegex.test(tempToken)) && tempToken !== '') {
           result.push(tempToken);
           tempToken = '';
         }
         tempToken += char;
       } else if (char === '.' || char === '-' && '+-*/^('.indexOf(result.at(-1)) >= 0) {
-        if (!(/^\d+$/.test(tempToken)) && tempToken !== '') {
+        if (!(/^\d$/.test(tempToken)) && tempToken !== '') {
           result.push(tempToken);
           tempToken = '';
         }
         tempToken += char;
       } else if (/\w/.test(char)) {
-        if (!(/^\w(?:\w|\d)*$/.test(tempToken)) && tempToken !== '') {
+        if (!(this.letterRegex.test(tempToken)) && tempToken !== '') {
           result.push(tempToken);
           tempToken = '';
         }
@@ -39,7 +44,7 @@ class NeungsuksaengLexer {
     this.position = 0;
   }
   getType(token) {
-    if (/^-?\d+(?:.\d*)?$/.test(token)) return 'number';
+    if (/^-?\d+(?:\.\d*)?$/.test(token)) return 'number';
     if (/^\w(?:\w|\d)*$/.test(token)) return 'identifier';
     if (token === '(') return 'open par';
     if (token === ')') return 'close par';
@@ -65,17 +70,69 @@ class NeungsuksaengCalculateParser {
     }
     throw new Error(`Unexpected token: %o`, ${this.currentToken});
   }
+  parseFunctionCall(tokenInput) {
+    let tokenRead = this.currentToken;
+    if (tokenInput.type === 'identifier') {
+      if (tokenRead.type === 'open par') {
+        this.readNext('open par');
+        const subexpression = this.parseExpression();
+        this.readNext('close par');
+        let result = { type: 'function call', functionName: tokenRead.name, input: subexpression };
+        return result;
+      }
+      return null;
+    }
+    return null;
+  }
   parseFactor() {
-    const tokenRead = this.currentToken;
+    let tokenRead = this.currentToken;
     if (tokenRead.type === 'number') {
       this.readNext('number');
       return { type: 'number', value: Number(tokenRead.token) };
+    } else if (tokenRead.type === 'identifier') {
+      this.readNext('identifier');
+      let result = { type: 'identifier', name: tokenRead.token };
+      tokenRead = this.currentToken;
+      if (tokenRead.type === 'open par') {
+        this.parseFunctionCall(result);
+      }
     } else if (tokenRead.type === 'open par') {
       this.readNext('open par');
       const subexpression = this.parseExpression();
       this.readNext('close par');
       return subexpression;
     }
+    return null;
+  }
+  parseExponent() {
+    let result = this.parseFactor();
+    while (this.currentToken == '^') {
+      const operator = this.currentToken;
+      this.readNext('operator');
+      const nextFactor = this.parseFactor();
+      result = { operator, left: result, right: nextFactor };
+    }
+    return result;
+  }
+  parseTerm() {
+    let result = this.parseExponent();
+    while (this.currentToken == '^') {
+      const operator = this.currentToken;
+      this.readNext('operator');
+      const nextExponent = this.parseExponent();
+      result = { operator, left: result, right: nextExponent };
+    }
+    return result;
+  }
+  parseExpression() {
+    let result = this.parseTerm();
+    while (this.currentToken == '+' || this.currentToken == '-') {
+      const operator = this.currentToken;
+      this.readNext('operator');
+      const nextTerm = this.parseTerm();
+      result = { operator, left: result, right: nextTerm };
+    }
+    return result;
   }
 }
 class NeungsuksaengClass {
@@ -83,7 +140,7 @@ class NeungsuksaengClass {
   }
   calculate(expr) {
     const tokens = parse(expr);
-    
+    // More code snippets should be added here for calculating expressions.
   }
 }
 const neungsuk = new NeungsuksaengClass();
